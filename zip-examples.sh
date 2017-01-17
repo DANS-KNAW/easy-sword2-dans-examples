ed7964cfc4fc7747010592e1afedcd05364fc408
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2017-01-17 17:57:57 +0100

    Fixed bug in zip-examples.sh

--- zip-examples.sh
+++ zip-examples.sh
@@ -6,6 +6,6 @@ ls -1 | xargs -I{} zip -r {}.zip {}
 popd
 
 pushd src/main/resources/invalid-examples
-ls -1 | grep "^.*(zip|txt)$" |  xargs -I{} rm {}
+ls -1 | grep "^.*zip$" |  xargs -I{} rm {}
 ls -1 | xargs -I{} zip -r {}.zip {}
 popd
